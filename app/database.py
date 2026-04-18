f70ecda4a0ddf9cab6d9801b87b812062035118f
paakshik <rbadjatya@gmail.com>
2026-04-18 19:53:56 +0530

    added the module change for the render deployment

--- app/database.py
+++ app/database.py
@@ -1,12 +1,12 @@
 from sqlmodel import SQLModel, create_engine, Session
 from app.config import DATABASE_URL
 
-DATABASE_URL = DATABASE_URL.replace(
+db_url = DATABASE_URL.replace(
     "postgresql://",
     "postgresql+psycopg2://"
 )
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(db_url, echo=False)
 
 def get_session():
     with Session(engine) as session:
